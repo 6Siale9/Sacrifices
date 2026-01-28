1d27808a7115ffacd39f0200271376359f2be05c
6Siale9 <gaspardodocuicui@gmail.com>
2026-01-28 14:18:07 +0100

    Debut de projet

---
+++ Assets/Scripts/RessourcesGaspard.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RessourcesGaspard 
+{
+    /*
+
+    https://www.youtube.com/watch?v=SMWxCpLvrcc
+      
+    */
+}
