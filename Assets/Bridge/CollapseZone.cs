5dbb39b9ae5b7bb86093739cf4fdc484a98c808b
Lenny BLONDEAU <lenny.blondeau@brassart.fr>
2026-02-24 12:25:27 +0100

    blazl

--- Assets/Bridge/CollapseZone.cs
+++ Assets/Bridge/CollapseZone.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
 
 public class CollapseZone : MonoBehaviour
 {
@@ -16,8 +17,9 @@ public class CollapseZone : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (_ants)
+        if (other.CompareTag("AllyAnt"))
         {
+            _currentUnits += other.GetComponent<Character>().Size;
             Destroy(other.gameObject);
             _currentUnits++;
 
