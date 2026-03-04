5acbd609021319576aaf93da3837da8a52643bb4
AdrienTerminal <adrien.terminal1@gmail.com>
2026-03-04 13:35:48 +0100

    controls

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    private float _moveSpeed = 20f;
+    private float _moveSpeed = 40f;
     private float _zoomSpeed = 10f;
     private float _minZoom = 0.4f;
     private float _maxZoom = 2.5f;
@@ -26,7 +26,12 @@ public class CameraController : MonoBehaviour
     {
         float horizontal = 0f;
         float vertical = 0f;
-
+        //_moveSpeed*= _scroll+5;
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            vertical -= Input.GetAxis("Mouse Y") * _moveSpeed * Time.deltaTime;
+            horizontal -= Input.GetAxis("Mouse X") * _moveSpeed * Time.deltaTime;
+        }
         if (Input.GetKey(KeyCode.Z))
             vertical += 1f;
         if (Input.GetKey(KeyCode.S))
@@ -38,7 +43,7 @@ public class CameraController : MonoBehaviour
 
         Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
 
-        transform.Translate(direction * _moveSpeed * Time.deltaTime, Space.World);
+        transform.Translate(direction * _moveSpeed * _scroll * Time.deltaTime, Space.World);
     }
     void Zoom()
     {
